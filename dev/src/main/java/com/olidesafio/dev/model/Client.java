c06c7b35bc1f5a6d871aa2c128da8b9336a6566b
Francine <francine.massaro@gmail.com>
2021-04-29 18:06:33 -0300

    feat: model client in english first version #1

---
+++ dev/src/main/java/com/olidesafio/dev/model/Client.java
@@ -0,0 +1,70 @@
+package com.olidesafio.dev.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private String sex;
+	
+	@NotNull
+	private String birthDate;
+	
+	//put date later here
+	
+	@OneToMany (mappedBy = "heathProblem",cascade = CascadeType.ALL)
+	@JsonIgnoreProperties ("heathProblem")
+	private List<HeathProblem> heathProblem;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+
+}
