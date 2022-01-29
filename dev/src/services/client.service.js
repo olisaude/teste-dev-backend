313ce665720cf64f7cb3aec9d0cc2e2ff38dd635
mariananbp <mariananbp@gmail.com>
2022-01-28 23:38:23 -0300

    client service

---
+++ dev/src/services/client.service.js
@@ -0,0 +1,51 @@
+const Joi = require('joi');
+const JoiDate = require('@hapi/joi-date');
+const errorHandling = require('../utils/functions/erroHandling');
+const { badRequest } = require('../utils/dictionary/statusCode');
+const { invalidEntry, invalidDate } = require('../utils/dictionary/messagesDefault');
+const { create } = require('../models/client.model');
+
+
+const clientSchema = Joi.object({
+  name: Joi.string().required(),
+  gender: Joi.string().required(),
+  health_problems: Joi.array().items(Joi.string()),
+});
+
+const dateSchema = Joi.object({
+  date: JoiDate.date().format('DD-MM-YYYY').required(),
+});
+
+const validateClient = (name, gender, health_problems) => {
+  const { error } = clientSchema.validate({
+    name,
+    gender,
+    health_problems,
+  });
+
+  if (error) throw errorHandling(badRequest, invalidEntry);
+};
+
+const validateDate = (date) => {
+  const { error } = clientSchema.validate({
+    date
+  });
+
+  if (error) throw errorHandling(badRequest, invalidDate);
+};
+
+const createClient = async (name, gender, health_problems, birth_date) => {
+  validateClient(name, gender, health_problems);
+  validateDate(birth_date);
+
+  /* const clientAlreadyExists = await findUserByEmail(email);
+
+  if (emailAlreadyExists) throw errorHandling(conflict, emailAlreadyRegistered); */
+
+  const id = await create(name, gender, health_problems, birth_date);
+  return id;
+};
+
+module.exports = {
+  createClient,
+};
