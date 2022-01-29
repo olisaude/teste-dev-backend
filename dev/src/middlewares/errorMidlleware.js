c18a9fd5d16d4cd0a2b23badc914f76d7bd63a23
mariananbp <mariananbp@gmail.com>
2022-01-29 18:59:47 -0300

    rename error middleware

--- dev/src/middlewares/errorMidlleware.js
+++ dev/src/middlewares/errorMidlleware.js
@@ -2,5 +2,6 @@ module.exports = (err, _req, res, _next) => {
   if (err.status) {
     return res.status(err.status).json({ message: err.message });
   }
+  console.log('middleware de erro: ', err)
   return res.status(500).json({ message: 'Internal Server Error' });
 };
