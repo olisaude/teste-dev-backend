ab7d6dcd4c48f0aee404b2fb259e50ffaad45236
mariananbp <mariananbp@gmail.com>
2022-01-29 19:02:45 -0300

    auxiliary functions

--- dev/src/utils/functions/currentDate.js
+++ dev/src/utils/functions/currentDate.js
@@ -1,7 +1,7 @@
 module.exports = () => {
   const dataAtual = new Date();
   const dia = dataAtual.getDate();
-  const mes = (dataAtual.getMonth()+1);
+  const mes = (dataAtual.getMonth() + 1);
   const ano = dataAtual.getFullYear();
   const date = `${dia}-${mes}-${ano}`;
   return date;
