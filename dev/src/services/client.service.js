0df4cfb71c9206fe26794f246065b4a4351ca48d
mariananbp <mariananbp@gmail.com>
2022-01-29 19:01:27 -0300

    client service - find client by id, name anda birth date

--- dev/src/services/client.service.js
+++ dev/src/services/client.service.js
@@ -1,25 +1,41 @@
 const Joi = require('joi').extend(require('@hapi/joi-date'));
-const errorHandling = require('../utils/functions/erroHandling');
-const { badRequest } = require('../utils/dictionary/statusCode');
-const { invalidEntry, invalidDate } = require('../utils/dictionary/messagesDefault');
-const { create } = require('../models/client.model');
-
+const errorHandling = require('../utils/functions/errorHandling');
+const { badRequest, notFound, conflict } = require('../utils/dictionary/statusCode');
+const {
+  invalidEntry,
+  invalidDate,
+  clientNotFound,
+  clientAlreadyRegistered
+} = require('../utils/dictionary/messagesDefault');
+const { create, getClientById, getClientByNameAndBirthDate } = require('../models/client.model');
 
 const clientSchema = Joi.object({
   name: Joi.string().required(),
   gender: Joi.string().required(),
-  health_problems: Joi.array().items(Joi.string()),
+  healthProblems: Joi.array().items(Joi.string()),
 });
 
 const dateSchema = Joi.object({
   date: Joi.date().format('DD-MM-YYYY'),
 });
 
-const validateClient = (name, gender, health_problems) => {
+const idSchema = Joi.object({
+  id: Joi.string().length(24).required(),
+});
+
+const validateClientId = (id) => {
+  const { error } = idSchema.validate({
+    id,
+  });
+
+  if (error) throw errorHandling(notFound, clientNotFound);
+};
+
+const validateClient = (name, gender, healthProblems) => {
   const { error } = clientSchema.validate({
     name,
     gender,
-    health_problems,
+    healthProblems,
   });
 
   if (error) throw errorHandling(badRequest, invalidEntry);
@@ -27,24 +43,43 @@ const validateClient = (name, gender, health_problems) => {
 
 const validateDate = (date) => {
   const { error } = dateSchema.validate({
-    date
+    date,
   });
 
   if (error) throw errorHandling(badRequest, invalidDate);
 };
 
-const createClient = async (name, gender, health_problems, birth_date, creation_date) => {
-  validateClient(name, gender, health_problems);
-  validateDate(birth_date);
+const createClient = async ({
+  name, gender, healthProblems, birthDate, creationDate,
+}) => {
+  validateClient(name, gender, healthProblems);
+  validateDate(birthDate);
 
-  /* const clientAlreadyExists = await findUserByEmail(email);
+  const clientAlreadyExists = await getClientByNameAndBirthDate(name, birthDate);
 
-  if (emailAlreadyExists) throw errorHandling(conflict, emailAlreadyRegistered); */
+  if (clientAlreadyExists) throw errorHandling(conflict, clientAlreadyRegistered);
 
-  const id = await create(name, gender, health_problems, birth_date, creation_date);
+  const id = await create(name, gender, healthProblems, birthDate, creationDate);
   return id;
+};
+
+const findClientById = async (id) => {
+  validateClientId(id);
+  const client = await getClientById(id);
+
+  if (!client) throw errorHandling(notFound, clientNotFound);
+  return client;
+};
+
+const findClientByNameAndBirthDate = async (name, birthDate) => {
+  const client = await getClientByNameAndBirthDate(name, birthDate);
+
+  if (!client) throw errorHandling(notFound, clientNotFound);
+  return client;
 };
 
 module.exports = {
   createClient,
+  findClientById,
+  findClientByNameAndBirthDate,
 };
