a5d10454fdddba1b8cbda4de2120584dc8c88042
mariananbp <mariananbp@gmail.com>
2022-01-29 19:01:00 -0300

    client model - find client by id, name anda birth date

--- dev/src/models/client.model.js
+++ dev/src/models/client.model.js
@@ -1,14 +1,33 @@
+const { ObjectId } = require('mongodb');
 const connect = require('./connection');
 
-const DB_COLLECTION = 'Clients';
+const DB_COLLECTION = 'clients';
 
-const create = async (name, birth_date, gender, health_problems, creation_date) => {
+const create = async (name, gender, healthProblems, birthDate, creationDate) => {
   const db = await connect();
   const { insertedId } = await db.collection(DB_COLLECTION)
-    .insertOne({ name, birth_date, gender, health_problems, creation_date });
+    .insertOne({
+      name, birthDate, gender, healthProblems, creationDate,
+    });
   return insertedId;
+};
+
+const getClientById = async (id) => {
+  const db = await connect();
+  const client = await db.collection(DB_COLLECTION)
+    .findOne({ _id: ObjectId(id) });
+  return client;
+};
+
+const getClientByNameAndBirthDate = async (name, birthDate) => {
+  const db = await connect();
+  const client = await db.collection(DB_COLLECTION)
+    .findOne({ name, birthDate });
+  return client;
 };
 
 module.exports = {
   create,
+  getClientById,
+  getClientByNameAndBirthDate,
 };
--- dev/src/models/connection.js
+++ dev/src/models/connection.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb').MongoClient;
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Clients';
-const DB_NAME = 'Clients';
+const MONGO_DB_URL = 'mongodb://localhost:27017/OliSaude';
+const DB_NAME = 'OliSaude';
 
 module.exports = () => mongodb.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
