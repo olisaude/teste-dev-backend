3286a9735bb1fe2c27f3608f8d8fd4b1344ac343
mariananbp <mariananbp@gmail.com>
2022-01-29 19:00:34 -0300

    add error midlleware in index.js

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const clientRouter = require('./routes/client.route');
+const errorMiddleware = require('./middlewares/errorMidlleware');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use('/', clientRouter);
+app.use('/clients', clientRouter);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
 
