8cf063265ed1774675dfff9ab12ce0a249a3e2cb
mariananbp <mariananbp@gmail.com>
2022-01-29 19:02:19 -0300

    dictionary

--- dev/src/utils/dictionary/messagesDefault.js
+++ dev/src/utils/dictionary/messagesDefault.js
@@ -1,4 +1,6 @@
 module.exports = {
   invalidEntry: 'Invalid entries. Try again.',
   invalidDate: 'Invalid format date. Please enter a date in MM-DD-YYYY format',
+  clientNotFound: 'Client not found',
+  clientAlreadyRegistered: 'This client is already registered',
 };
