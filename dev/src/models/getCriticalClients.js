4752b18535a8e8fba517a202f7b3bdd1e1564a0c
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-02-01 17:16:59 -0300

    resolve calc error

--- dev/src/models/getCriticalClients.js
+++ dev/src/models/getCriticalClients.js
@@ -8,8 +8,8 @@ module.exports = async () => {
       (acc, problem) => acc + problem.level,
       0,
     );
-
-    const score = (1 / (1 + Math.E, -(2.8 + problemSum / client.problems.length))) * 100;
+      
+    const score = (1 / (1 + Math.exp(-(-2.8 + problemSum)))) * 100;
     return { ...client, score };
   });
   return getCriticalClients;
