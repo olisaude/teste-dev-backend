c88970a59b42db9aeeffa492d79c9aa8ff8a4d7b
mariananbp <mariananbp@gmail.com>
2022-02-01 22:45:19 -0300

    find client by id - test

---
+++ dev/src/tests/findClientById.test.js
@@ -0,0 +1,99 @@
+const chai = require('chai');
+const {
+  describe, it, before, after,
+} = require('mocha');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../api/index');
+const clients = require('./utils/clients');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('GET /clients/:id', () => {
+  describe('quando ocliente é encontrado com sucesso', () => {
+    let clientId;
+    let response = {};
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(
+        URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+      const client = await chai.request(app).post('/clients').send(clients[0]);
+
+      const { _id } = client.body;
+      clientId = _id;
+
+      response = await chai.request(app)
+        .get(`/clients/${_id}`);
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('retorna o código de status 200', () => {
+      expect(response).to.have.status(200);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.a('object');
+    });
+  });
+
+  describe('Quando o payload é inválido', () => {
+    let response = {};
+
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(
+        URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      response = await chai.request(app)
+        .post('/clients')
+        .send({});
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('recebe o status 400', () => {
+      expect(response).to.have.status(400);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it(
+      'a propriedade "message" possui o texto "Invalid entries. Try again."',
+      () => {
+        expect(response.body.message)
+          .to.be.equal('Invalid entries. Try again.');
+      },
+    );
+  });
+});
