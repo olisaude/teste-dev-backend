3697e31495dc6089a7dcf43e9cc8c4b8fc59d15b
mariananbp <mariananbp@gmail.com>
2022-02-01 23:14:13 -0300

    get all clients - test

---
+++ dev/src/tests/findAllClients.test.js
@@ -0,0 +1,95 @@
+const chai = require('chai');
+const {
+  describe, it, before, after,
+} = require('mocha');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../api/index');
+const clients = require('./utils/clients');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('GET /clients/:id', () => {
+  describe('quando os clientes são encontrados com sucesso', () => {
+    let response;
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(
+        URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+      await chai.request(app).post('/clients').send(clients[0]);
+
+      response = await chai.request(app)
+        .get('/clients/all');
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('retorna o código de status 200', () => {
+      expect(response).to.have.status(200);
+    });
+
+    it('retorna um array', () => {
+      expect(response.body).to.be.a('array');
+    });
+  });
+
+  describe('Quando não há clientes cadastrados', () => {
+    let response;
+
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(
+        URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+      await chai.request(app).post('/clients').send();
+
+      response = await chai.request(app)
+        .get('/clients/all');
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('recebe o status 404', () => {
+      expect(response).to.have.status(404);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.a('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it(
+      'a propriedade "message" possui o texto "There are no clients registered"',
+      () => {
+        expect(response.body.message)
+          .to.be.equal('There are no clients registered');
+      },
+    );
+  });
+});
