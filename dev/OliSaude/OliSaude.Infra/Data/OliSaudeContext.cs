65e5c904d947ca24e558510140eedc120b4048f3
RaMadaSilva <raul01mateia@gmail.com>
2023-11-03 00:07:46 +0100

    criando o repositorio

---
+++ dev/OliSaude/OliSaude.Infra/Data/OliSaudeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OliSaude.Domain.Entities;
+
+namespace OliSaude.Infra.Data; 
+
+public class OliSaudeContext : DbContext 
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    public OliSaudeContext(DbContextOptions<OliSaudeContext> options )
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ dev/OliSaude/OliSaude.Infra/Repostorio/ClienteRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OliSaude.Application.Interfaces;
+using OliSaude.Domain.Entities;
+using OliSaude.Infra.Data;
+
+namespace OliSaude.Infra.Repostorio;
+public class ClienteRepositorio : IClienteRepositorio
+{
+    private readonly OliSaudeContext _context;
+
+    public ClienteRepositorio(OliSaudeContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Cliente> GetAllClientes()
+    {
+        return _context
+                .Clientes
+                .AsNoTracking()
+                .ToList(); 
+    }
+
+    public Cliente GetCliente(int id)
+    {
+       var cliente =  _context.Clientes.FirstOrDefault(c => c.Id == id);
+        return cliente; 
+    }
+
+    public int Save(Cliente cliente)
+    {
+        _context.Add(cliente);
+        _context.SaveChanges();
+
+        return cliente.Id;
+    }
+
+    public void UpdateCliente(int id)
+    {
+       var cliente = _context.Clientes.FirstOrDefault(x => x.Id == id);
+        _context.Entry(cliente).State = EntityState.Modified; 
+        _context.SaveChanges();
+    }
+
+    public void DeleteCliente(int id)
+    {
+        var cliente = _context.Clientes.FirstOrDefault(x=>x.Id ==id);
+        _context.Remove(cliente);
+        _context.SaveChanges(); 
+    }
+}
