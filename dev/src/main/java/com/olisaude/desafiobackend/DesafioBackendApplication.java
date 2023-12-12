d0f1fadecadd91600fb07e610d9e5c5bcf99facb
Lamark <lamark12ricarte@gmail.com>
2023-12-12 16:36:12 -0300

    first commit

---
+++ dev/src/main/java/com/olisaude/desafiobackend/DesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.olisaude.desafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendApplication.class, args);
+	}
+
+}
